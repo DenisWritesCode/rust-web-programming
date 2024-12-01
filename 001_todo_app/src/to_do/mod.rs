93a7628d23d3e0c32c44ae9b6945ec469ff0209c
DenisWritesCode <deniswritescode@gmail.com>
2024-12-01 08:18:45 +0300

    c-02: Add a factory for generating to-do items

--- 001_todo_app/src/to_do/mod.rs
+++ 001_todo_app/src/to_do/mod.rs
@@ -3,3 +3,28 @@ pub mod structs;
 
 // Make enum in to_do/enums.rs publicly available as a module.
 pub mod enums;
+
+// Use TaskStatus enum + Done & Pending structs. 
+use enums::TaskStatus;
+use structs::done::Done;
+use structs::pending::Pending;
+
+// All Possible To-Do Items for now. They are largely the same but differ in status.
+pub enum ItemTypes {
+    Pending(Pending),
+    Done(Done)
+}
+
+// Generator of To-Do's.
+// Accepts title and status as input.
+// Returns an Item with the correct initialization.
+pub fn to_do_factory(title: &str, status: TaskStatus) -> ItemTypes {
+    match status {
+        TaskStatus::DONE => {
+            ItemTypes::Done(Done::new(title))
+        },
+        TaskStatus::PENDING => {
+            ItemTypes::Pending(Pending::new(title))
+        }
+    }
+}
