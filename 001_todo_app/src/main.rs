f622c0f77ac7e379aa321cb0c46bb51c272d68d3
DenisWritesCode <deniswritescode@gmail.com>
2024-12-02 09:48:04 +0300

    c-02: Done with Reading and Writing JSON files

--- 001_todo_app/src/main.rs
+++ 001_todo_app/src/main.rs
@@ -1,29 +1,20 @@
-mod to_do;
-use to_do::to_do_factory;
-use to_do::enums::TaskStatus;
-use to_do::ItemTypes;
+mod state;
 
-use crate::to_do::traits::get::Get;
-use crate::to_do::traits::delete::Delete;
-use crate::to_do::traits::edit::Edit;
+use std::env;
+use state::{write_to_file, read_file};
+use serde_json::value::Value;
+use serde_json::{Map, json};
 
 fn main() {
-    let to_do_items = to_do_factory("washing", TaskStatus::DONE);
-    match to_do_items {
-        ItemTypes::Done(item) => {
-            item.get(&item.super_struct.title);
-            item.delete(&item.super_struct.title);
-        },
-        ItemTypes::Pending(item) => {
-            item.get(&item.super_struct.title);
-            item.set_to_done(&item.super_struct.title);
-        }
-    }
-    // let done = Done::new("shopping");
-    // println!("{}", done.super_struct.title);
-    // println!("{}", done.super_struct.status.stringify());
+    let args: Vec<String> = env::args().collect();
+    let status: &String = &args[1];
+    let title: &String = &args[2];
 
-    // let pending = Pending::new("laundry");
-    // println!("{}", pending.super_struct.title);
-    // println!("{}", pending.super_struct.status.stringify());
+    let mut state: Map<String, Value> = read_file("./state.json");
+    println!("Before operation: {:?}", state);
+
+    state.insert(title.to_string(), json!(status));
+    println!("After operation: {:?}", state);
+
+    write_to_file("./state.json", &mut state);
 }
--- 001_todo_app/src/state.rs
+++ 001_todo_app/src/state.rs
@@ -12,7 +12,7 @@ pub fn read_file(file_name: &str) -> Map<String, Value> {
     let mut data = String::new();
     file.read_to_string(&mut data).unwrap();
 
-    let json: Value = serde_json::from_Str(&data).unwrap();
+    let json: Value = serde_json::from_str(&data).unwrap();
     let state: Map<String, Value> = json.as_object().unwrap().clone();
 
     return state;
