b33fcf6e5e3a3de3301b0e41c18a6016de992916
DenisWritesCode <deniswritescode@gmail.com>
2024-12-05 15:11:33 +0300

    chapter-04: done integrating serialization structs

--- 004_processing_http_requests/src/to_do/enums.rs
+++ 004_processing_http_requests/src/to_do/enums.rs
@@ -1,8 +1,25 @@
-use serde::ser::{SeializeStruct, Serialize, Serializer};
+use serde::ser::{Serialize, SerializeStruct, Serializer};
 
 pub enum TaskStatus {
     DONE,
     PENDING,
+}
+
+impl TaskStatus {
+    pub fn stringify(&self) -> String {
+        match &self {
+            &Self::DONE => "DONE".to_string(),
+            &Self::PENDING => "PENDING".to_string(),
+        }
+    }
+
+    pub fn from_string(input_string: String) -> Self {
+        match input_string.as_str() {
+            "DONE" => TaskStatus::DONE,
+            "PENDING" => TaskStatus::PENDING,
+            _ => panic!("input {} not supported", input_string),
+        }
+    }
 }
 
 impl Serialize for TaskStatus {
@@ -10,6 +27,8 @@ impl Serialize for TaskStatus {
     where
         S: Serializer,
     {
-        Ok(serializer.serialize_str(&self.stringify().as_str())?)
+        let mut s = serializer.serialize_struct("TaskStatus", 1)?;
+        s.serialize_field("status", &self.stringify())?;
+        s.end()
     }
 }
