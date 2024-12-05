afabd6e084b7b6afd56f4889ec082e7a35556e76
DenisWritesCode <deniswritescode@gmail.com>
2024-12-05 17:09:37 +0300

    chapter-04: done extracting json from request body

--- 004_processing_http_requests/src/to_do/traits/delete.rs
+++ 004_processing_http_requests/src/to_do/traits/delete.rs
@@ -5,8 +5,7 @@ use crate::state::write_to_file;
 
 
 pub trait Delete {
-    fn delete(&self, title: &String,
-              state: &mut Map<String, Value>) {
+    fn delete(&self, title: &String, state: &mut Map<String, Value>) {
         state.remove(title);
         write_to_file("./state.json", state);
         println!("\n\n{} is being deleted\n\n", title);
