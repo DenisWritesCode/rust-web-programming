4d11b3201c7f99ed08ba3895a1f74f2be154fa84
DenisWritesCode <deniswritescode@gmail.com>
2024-12-05 11:59:09 +0300

    chapter-04: done using macros for JSON serialization

--- 004_processing_http_requests/src/views/to_do/mod.rs
+++ 004_processing_http_requests/src/views/to_do/mod.rs
@@ -1,11 +1,13 @@
 mod create;
+mod get;
 
-use actix_web::web::{get, post, scope, ServiceConfig};
+use actix_web::web::{ServiceConfig, post, scope, get};
+
 
 pub fn to_do_views_factory(app: &mut ServiceConfig) {
     app.service(
         scope("v1/item")
             .route("create/{title}", post().to(create::create))
-            .route("create/{title}", get().to(create::create)),
+            .route("get", get().to(get::get)) // define view and URL
     );
 }
