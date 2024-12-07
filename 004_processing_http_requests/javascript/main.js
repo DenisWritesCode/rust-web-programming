519353af3f074022c55dd7197636d9380150a4b1
DenisWritesCode <deniswritescode@gmail.com>
2024-12-07 08:42:33 +0300

    chapter-05: done building a rendering js function

---
+++ 004_processing_http_requests/javascript/main.js
@@ -0,0 +1,28 @@
+function renderItems(items, processType, elementId, processFunction) {
+  let itemsMeta = [];
+  let placeholder = "<div>";
+
+  for (let i = 0; i < items.length; i++) {
+    let title = items[i]["title"];
+    let placeholderId = processType + "-" + title.replaceAll(" ", "-");
+    placeholder +=
+      "<div>" +
+      title +
+      "<button " +
+      'id="' +
+      placeholderId +
+      '">' +
+      processType +
+      "</button>" +
+      "</div>";
+    itemsMeta.push({ id: placeholderId, title: title });
+  }
+  placeholder += "</div>";
+  document.getElementById(elementId).innerHTML = placeholder;
+
+  for (let i = 0; i < itemsMeta.length; i++) {
+    document
+      .getElementById(itemsMeta[i]["id"])
+      .addEventListener("click", processFunction);
+  }
+}
