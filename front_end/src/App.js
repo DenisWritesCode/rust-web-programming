ec151cb53c58b26ae8f729197e7978da74b359dc
DenisWritesCode <deniswritescode@gmail.com>
2024-12-07 16:17:48 +0300

    chapter-05: done constructing and managing custom components

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+import ToDoItem from "./components/ToDoItem";
+import CreateToDoItem from "./components/CreateToDoItem";
+
 class App extends Component {
   state = {
     pending_items: [],
@@ -38,23 +41,38 @@ class App extends Component {
     let itemList = [];
     items.forEach((item, index) => {
       itemList.push(
-        <li key={index}>
-          {item.title} {item.status.status}
-        </li>
+        <ToDoItem
+          key={item.title + item.status}
+          title={item.title}
+          status={item.status.status}
+          passBackResponse={this.handleReturnedState}
+        />
       );
     });
     return itemList;
   }
 
+  handleReturnedState = (response) => {
+    let pending_items = response.data["pending_items"];
+    let done_items = response.data["done_items"];
+    this.setState({
+      pending_items: this.processItemValues(pending_items),
+      done_items: this.processItemValues(done_items),
+      pending_items_count: response.data["pending_item_count"],
+      done_items_count: response.data["done_item_count"],
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>Done Items</h1>
-        <p>done item count: {this.state.done_items_count}</p>
-        {this.state.done_items}
         <h1>Pending Items</h1>
         <p>pending item count: {this.state.pending_items_count}</p>
         {this.state.pending_items}
+        <h1>Done Items</h1>
+        <p>done item count: {this.state.done_items_count}</p>
+        {this.state.done_items}
+        <CreateToDoItem passBackResponse={this.handleReturnedState} />
       </div>
     );
   }
