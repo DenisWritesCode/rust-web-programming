59f99c59a763fa49756c45e08b6848842a734b70
DenisWritesCode <deniswritescode@gmail.com>
2024-12-08 12:18:15 +0300

    chapter-06: done creating our data models

--- 005_data_persistence/004_processing_http_requests/src/main.rs
+++ 005_data_persistence/004_processing_http_requests/src/main.rs
@@ -1,29 +1,41 @@
-use actix_web::{App, HttpServer};
-use actix_service::Service;
-use actix_cors::Cors;
+use diesel::{prelude::*, result::Error};
+use dotenv::dotenv;
 
-mod views;
-mod to_do;
-mod state;
-mod processes;
+use actix_cors::Cors;
+use actix_service::Service;
+use actix_web::{App, HttpServer};
+
+mod database;
 mod json_serialization;
 mod jwt;
+mod models;
+mod processes;
+mod schema;
+mod state;
+mod to_do;
+mod views;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        let cors = Cors::default().allow_any_origin().allow_any_method().allow_any_header();
-        let app = App::new().wrap_fn(|req, srv|{
-            println!("{}-{}", req.method(), req.uri());
-            let future = srv.call(req);
-            async {
-                let result = future.await?;
-                Ok(result)
-            }
-        }).configure(views::views_factory).wrap(cors);
-        return app
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allow_any_method()
+            .allow_any_header();
+        let app = App::new()
+            .wrap_fn(|req, srv| {
+                println!("{}-{}", req.method(), req.uri());
+                let future = srv.call(req);
+                async {
+                    let result = future.await?;
+                    Ok(result)
+                }
+            })
+            .configure(views::views_factory)
+            .wrap(cors);
+        return app;
     })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
